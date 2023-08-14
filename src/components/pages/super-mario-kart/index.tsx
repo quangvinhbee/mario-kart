f71835127ac3d12d88751513c41351b20fb0cde5
Anh Tran Duc <anh.td1401@gmail.com>
2023-08-14 23:39:02 +0700

    init

--- src/components/pages/super-mario-kart/index.tsx
+++ src/components/pages/super-mario-kart/index.tsx
@@ -16,43 +16,55 @@ export const SuperMarioKart = () => {
   const yochiRef = useRef<HTMLDivElement>(null)
 
   const raceStatus = useRef([
-    { ref: bowserRef, speed: 4 },
-    { ref: dkRef, speed: 9 },
-    { ref: goombaRef, speed: 16 },
-    { ref: luigiRef, speed: 7 },
-    { ref: marioRef, speed: 6 },
-    { ref: peachRef, speed: 5 },
-    { ref: toadRef, speed: 4 },
-    { ref: yochiRef, speed: 14 },
+    { name: 'luigi', ref: luigiRef, speed: 7, bottom: 108 },
+    { name: 'mario', ref: marioRef, speed: 6, bottom: 97 },
+    { name: 'bowser', ref: bowserRef, speed: 4, bottom: 86 },
+    { name: 'goomba', ref: goombaRef, speed: 16, bottom: 75 },
+    { name: 'peach', ref: peachRef, speed: 5, bottom: 64 },
+    { name: 'yoshi', ref: yochiRef, speed: 14, bottom: 53 },
+    { name: 'dk', ref: dkRef, speed: 9, bottom: 42 },
+    { name: 'toad', ref: toadRef, speed: 4, bottom: 32 },
   ])
 
+  const handleClick = (index: number) => {
+    if (raceStatus.current[index]) {
+      const el = raceStatus.current[index].ref.current
+      el.classList.remove(cx('running'))
+      el.classList.add(cx('spin'))
+    }
+  }
+
   useEffect(() => {
-    console.log(containerRef)
     function animate() {
       if (!containerRef.current) {
-        // window.requestAnimationFrame(animate)
         return
       }
       for (const p of raceStatus.current) {
         if (!p.ref.current) {
           continue
         }
+        const el = p.ref.current
+        const isRunning = el.classList.contains(cx('running'))
+        console.log(isRunning)
         const containerWidth = containerRef.current.offsetWidth
-        const left = p.ref.current.offsetLeft
-        if (left - containerWidth > 200) {
-          p.ref.current.style.transitionDuration = '0ms'
-          p.ref.current.style.left = '-300px'
+        const left = el.offsetLeft
+        if (isRunning) {
+          if (left - containerWidth > 200) {
+            el.style.transitionDuration = '0ms'
+            el.style.left = '-300px'
+          } else if (left < -400) {
+            el.classList.remove(cx('spin'))
+            el.classList.add(cx('running'))
+          } else {
+            const distance = p.speed
+            el.style.transitionDuration = '150ms'
+            el.style.left = left + distance + 'px'
+          }
         } else {
-          const distance = p.speed
-          p.ref.current.style.transitionDuration = '150ms'
-          p.ref.current.style.left = left + distance + 'px'
+          el.style.left = left - 30 + 'px'
         }
-        // console.log(p.ref)
       }
-
-      // window.requestAnimationFrame(animate)
     }
-    // window.requestAnimationFrame(animate)
     const interval = setInterval(animate, 150)
     return () => {
       clearInterval(interval)
@@ -61,13 +73,28 @@ export const SuperMarioKart = () => {
 
   return (
     <div ref={containerRef} className={cx('background', 'relative h-full w-full overflow-hidden')}>
-      <div
+      {raceStatus.current.map((item, i) => (
+        <div
+          ref={item.ref}
+          className={cx(
+            item?.name,
+            'running',
+            'absolute left-[-300px] h-[93px] w-[100px] cursor-pointer transition-all ease-linear'
+          )}
+          style={{
+            bottom: item?.bottom + 'px',
+          }}
+          onClick={() => handleClick(i)}
+        />
+      ))}
+      {/* <div
         ref={bowserRef}
         className={cx(
           'bowser',
           'running',
           'absolute left-[-300px] bottom-[86px] h-[93px] w-[100px] transition-all ease-linear'
         )}
+        onClick={() => handleClick(0)}
       />
       <div
         ref={dkRef}
@@ -76,6 +103,7 @@ export const SuperMarioKart = () => {
           'running',
           'absolute left-[-300px] bottom-[42px] h-[93px] w-[100px] transition-all ease-linear'
         )}
+        onClick={() => handleClick(1)}
       />
       <div
         ref={goombaRef}
@@ -84,6 +112,7 @@ export const SuperMarioKart = () => {
           'running',
           'absolute left-[-300px] bottom-[75px] h-[93px] w-[100px] transition-all ease-linear'
         )}
+        onClick={() => handleClick(2)}
       />
       <div
         ref={luigiRef}
@@ -92,6 +121,7 @@ export const SuperMarioKart = () => {
           'running',
           'absolute left-[-300px] bottom-[108px] h-[93px] w-[100px] transition-all ease-linear'
         )}
+        onClick={() => handleClick(3)}
       />
       <div
         ref={marioRef}
@@ -100,6 +130,7 @@ export const SuperMarioKart = () => {
           'running',
           'absolute left-[-300px] bottom-[97px] h-[93px] w-[100px] transition-all ease-linear'
         )}
+        onClick={() => handleClick(4)}
       />
       <div
         ref={peachRef}
@@ -124,7 +155,7 @@ export const SuperMarioKart = () => {
           'running',
           'absolute left-[-300px] bottom-[53px] h-[93px] w-[100px] transition-all ease-linear'
         )}
-      />
+      /> */}
     </div>
   )
 }
