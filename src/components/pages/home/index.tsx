7e82195aa79246476bc608346f7f04616739734a
Anh Tran Duc <anh.td1401@gmail.com>
2023-11-26 10:51:27 +0700

    update home page

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -86,15 +86,37 @@ export const HomePage = () => {
       <div className="fixed inset-0">
         <RacePreview />
       </div>
-      <div className="pointer-events-none relative flex w-full flex-col items-center pt-[80px]">
-        <Image className="" src="/assets/game/banner.png" alt="" width={646} height={432} />
-        <div className="pointer-events-auto translate-y-[-100%]">
-          <Link href="/play">
-            <img className="cursor-pointer" src="/assets/game/button-play.png" alt="" />
+      <div className="pointer-events-none relative w-full pt-[80px]">
+        <div className="max-w-[645px] mx-auto">
+          <Image className="" src="/assets/game/banner.png" alt="" width={646} height={432} />
+          <div className="pointer-events-auto translate-y-[-100%]">
+            <Link href="/play">
+              <img className="cursor-pointer mx-auto max-w-[337px] w-full" src="/assets/game/button-play.png" alt="" />
+            </Link>
+            <Link href="/help">
+              <a className="mt-[4px] block text-center uppercase underline underline-offset-2">
+                How to play
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="pointer-events-auto absolute left-[64px] top-[120px] space-y-[24px]">
+          <Link href={'#'}>
+            <a className="flex items-center space-x-[8px] cursor-pointer" target="_blank">
+              <img className="w-[32px]" src="/assets/game/ic-arrow-right.png" alt="" />
+              <p className="uppercase">buy token</p>
+            </a>
           </Link>
-          <Link href="/help">
-            <a className="mt-[4px] block text-center uppercase underline underline-offset-2">
-              How to play
+          <Link href={'#'}>
+            <a className="flex items-center space-x-[8px] cursor-pointer" target="_blank">
+              <img className="w-[32px]" src="/assets/game/ic-arrow-right.png" alt="" />
+              <p className="uppercase">view chart</p>
+            </a>
+          </Link>
+          <Link href={'#'}>
+            <a className="flex items-center space-x-[8px] cursor-pointer" target="_blank">
+              <img className="w-[32px]" src="/assets/game/ic-arrow-right.png" alt="" />
+              <p className="uppercase">coinmarketcap</p>
             </a>
           </Link>
         </div>
