caeac22249882eb3d233760ae22de724714782ce
Anh Tran Duc <anh.td1401@gmail.com>
2023-12-05 17:28:13 +0700

    update seo

--- src/configs/next-seo.config.js
+++ src/configs/next-seo.config.js
@@ -1,32 +1,32 @@
 export default {
-  title: 'Mario Cart',
+  title: '0xRace Erc-20',
   titleTemplate: '%s',
-  canonical: '',
-  description: `Mario Cart`,
+  description: `A childhood is brought into 0xRace where Mario and his iconic friends clash in an electrifying kart racing arena, fused with the strategic thrills of $0xRace betting.
+  Race to victory, bet in $0xRace, Conquer the Race`,
   additionalMetaTags: [
     {
       name: 'keywords',
-      content: 'Mario Cart',
+      content: '0xRace Erc-20',
     },
   ],
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: '',
-    site_name: 'Mario Cart',
+    site_name: '0xRace Erc-20',
     images: [
       {
         url: '/images/image-seo.jpg',
         width: 800,
         height: 600,
-        alt: 'Mario Cart',
+        alt: '0xRace Erc-20',
         type: 'image/*',
       },
       {
         url: '/images/image-seo.jpg',
         width: 900,
         height: 800,
-        alt: 'Mario Cart',
+        alt: '0xRace Erc-20',
         type: 'image/*',
       },
     ],
