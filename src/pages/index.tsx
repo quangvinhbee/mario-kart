a3d25cad09cefd7cf428302ef317c80f64362787
Anh Tran Duc <anh.td1401@gmail.com>
2023-12-06 20:39:43 +0700

    update seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { GameLayout } from '@/components/layouts/GameLayout'
 import { HomePage } from '@/components/pages/home'
+import { NextSeo } from 'next-seo'
 
 const Page = () => {
-  return <HomePage />
+  return (
+    <>
+      <NextSeo title="How to play | 0xRace Erc-20" />
+      <HomePage />
+    </>
+  )
 }
 
 Page.Layout = GameLayout
