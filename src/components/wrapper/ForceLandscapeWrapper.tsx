4e9302f056587d21372367460d822a9211d966d1
Anh Tran Duc <anh.td1401@gmail.com>
2023-12-06 17:26:41 +0700

    rotate

--- src/components/wrapper/ForceLandscapeWrapper.tsx
+++ src/components/wrapper/ForceLandscapeWrapper.tsx
@@ -11,14 +11,15 @@ export default function ForceLandscapeWrapper({ children }: ThemeWrapperProps) {
     return
   }
 
-  if (height > width) {
-    return (
-      <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
-        <img className='w-[80%]' src="/assets/common/rotate-device.gif" alt="" />
-        <p className='text-white text-[60px] font-semibold'>Please rotate your device</p>
-      </div>
-    )
-  }
-
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      {height > width && (
+        <div className="fixed inset-0 z-[9999999999] flex flex-col justify-center items-center bg-black">
+          <img className="w-[80%]" src="/assets/common/rotate-device.gif" alt="" />
+          <p className="text-white text-[60px] font-semibold">Please rotate your device</p>
+        </div>
+      )}
+    </>
+  )
 }
